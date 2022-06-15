e569103f8d549215f7727efae60eef38443e2a2f
Lenin <leninmanueld@gmail.com>
2022-06-15 10:20:40 -0400

    copying proyect for graphql

--- src/custom.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.svg" {
-    import React =  require("react");
-    export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
-    const src: string;
-    export default src;
-};
-
